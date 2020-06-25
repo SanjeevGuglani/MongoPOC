2308539ce207e2eb18e7d5a050864d7d91199187
Sanjeev Guglani <guglanisanjeev7@gmail.com>
2020-06-25 09:39:04 +0530

    POC Changes

--- complete/src/main/java/com/example/accessingdatamongodb/CustomerRepository.java
+++ complete/src/main/java/com/example/accessingdatamongodb/CustomerRepository.java
@@ -3,10 +3,14 @@ package com.example.accessingdatamongodb;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
 	public Customer findByFirstName(String firstName);
 	public List<Customer> findByLastName(String lastName);
+
+	@Query(value = "{ firstName: ?0 }")
+	public Customer findOne(String customerId);
 
 }
